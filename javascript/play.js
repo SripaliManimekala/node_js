0884b6ad170aa0253373326e23410885d6f1370a
SripaliManimekala <sripalimanimekala@gmail.com>
2023-07-30 11:16:10 +0530

    added a function

--- javascript/play.js
+++ javascript/play.js
@@ -1,2 +1,15 @@
 var name='max';
+var age =28;
+var hasHobbies=true;
 console.log(name);
+
+function summarizeUser(userName,useAge,useHasHobby){
+
+    return (
+        'name is ' +userName +
+        'age is ' +useAge+
+        'and the user has hobbies: '+hasHobbies
+        );
+}
+
+console.log(summarizeUser);
