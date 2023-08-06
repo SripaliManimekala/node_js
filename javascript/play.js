54d2b5c6852898b09d0529b3341df0a89e66fe0c
SripaliManimekala <sripalimanimekala@gmail.com>
2023-08-06 22:32:09 +0530

    added a arrow function called addOne

--- javascript/play.js
+++ javascript/play.js
@@ -14,5 +14,8 @@ const summarizeUser = (userName,userAge,userHasHobby) => {
 
 const add = (a, b) => a+b;
 
+const addOne = (a) => a+1;
+
+console.log(addOne(4));
 console.log(add(1,2));
 console.log(summarizeUser(name,age,hasHobbies));
