44ed67d4cb69f8e8d63ed022a5861300ffb01e70
SripaliManimekala <sripalimanimekala@gmail.com>
2023-09-17 13:39:35 +0530

    async code

--- javascript/play3.js
+++ javascript/play3.js
@@ -4,5 +4,11 @@ console.log(2);
 
 setTimeout(()=>{console.log('callback function is fired')}, 2000);
 
+
 console.log(3);
 console.log(4);
+
+setTimeout(()=>{console.log('second callback function fired')},5000);
+
+console.log(5);
+console.log(6);
