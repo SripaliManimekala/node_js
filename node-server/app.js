e99ae4470d029a89aeb5ba47e08a13438e87effc
SripaliManimekala <sripalimanimekala@gmail.com>
2023-10-16 08:44:00 +0530

    understanding requests

--- node-server/app.js
+++ node-server/app.js
@@ -1,3 +1,7 @@
 const http = require('http');
 
-http.createServer;
+const server = http.createServer((req, res)=>{
+    console.log(req.url, req.method, req.headers);
+    process.exit();
+});
+server.listen(3000);
