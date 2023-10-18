283c3a299df06650c244faf1b2258f1036b40f19
SripaliManimekala <sripalimanimekala@gmail.com>
2023-10-19 01:22:17 +0530

    understanding responses

--- node-server/app.js
+++ node-server/app.js
@@ -2,6 +2,12 @@ const http = require('http');
 
 const server = http.createServer((req, res)=>{
     console.log(req.url, req.method, req.headers);
-    process.exit();
+    //process.exit();
+    res.setHeader('content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>my first page</title></head>');
+    res.write('<body>hello from server</body>')
+    res.write('</html>');
+    res.end();
 });
 server.listen(3000);
